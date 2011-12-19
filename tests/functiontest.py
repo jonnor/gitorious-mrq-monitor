29b27cfe1110f8f7769d1da2a00f60c15531fbef
Jon Nordby <jononor@gmail.com>
2011-12-19 16:09:50 +0100

    Add start of some tests
    Currenly only a sanity check and a manual function test.

---
+++ tests/functiontest.py
@@ -0,0 +1,76 @@
+import os
+
+from twisted.web import server, resource
+from twisted.internet import reactor, protocol
+
+import gitorious_mrq
+import gitorious_mrq.ircbot
+
+"""Verify the essential functionality of the bot."""
+
+# TODO: make into an automated test
+# Either
+# 1. Implement channels for Twisted Words IRC server
+# 2. Find another Python IRC server implementation
+#
+# Then hook up the IRC server in code and verify behavior programatically
+
+monitor_executable = 'gitorious-mrq-monitor'
+
+class Feed(resource.Resource):
+    isLead = True
+    
+    def __init__(self):
+        resource.Resource.__init__(self)
+        self.request_no = 0
+    
+    def render_GET(self, request):
+        
+        # FIXME: unhardcode, make more general
+        self.request_no += 1
+        if self.request_no > 4:
+            self.request_no = 1
+        return open("tests/data/maliit.atom.%d.txt" % self.request_no).read()
+
+
+class MonitorProcessProtocol(protocol.ProcessProtocol):
+
+    def __init__(self):
+        pass
+
+
+def run_mrq_monitor():
+    
+    args = [monitor_executable, 'maliit',
+            '--host=http://localhost:8080', '--poll-interval=15', '--irc-channel=#gitorious-mrq-monitor-test',]
+    executable = monitor_executable
+    
+    #executable = 'which'
+    #args = [executable, 'gitorious-mrq-monitor']
+    print args
+    
+    # Hook up child process file descriptions to the parent
+    childFDs = { 0: 0, 1: 1, 2: 2}
+    
+    # FIXME: refactory gitorious-mrq-monitor such that we can call it directly as Python here?
+
+    processProtocol = MonitorProcessProtocol()
+    reactor.spawnProcess(processProtocol, executable, args=args, childFDs=childFDs, env=os.environ)
+
+def setup_feedserver():
+
+    root = Feed()
+    feed = Feed()
+    root.putChild("maliit.atom", feed)
+    site = server.Site(root)
+    reactor.listenTCP(8080, site)
+
+def setup_monitor():
+    reactor.callLater(3, run_mrq_monitor)
+
+if __name__ == '__main__':
+
+    setup_feedserver()  
+    setup_monitor()
+
+    reactor.run()
---
+++ tests/test_sanity.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestSanity(unittest.TestCase):
+
+    def test_import(self):
+        import gitorious_mrq
+
+if __name__ == '__main__':
+    unittest.main()
