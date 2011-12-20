903f4487997e3c043937576044902d93bf9ce350
Jon Nordby <jononor@gmail.com>
2011-12-20 07:35:11 +0100

    Add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,20 @@
 
 from distutils.core import setup
 
+classifiers = """\
+Development Status :: 4 - Beta
+Intended Audience :: Developers
+Programming Language :: Python
+Programming Language :: Python :: 2
+License :: OSI Approved :: GNU General Public License (GPL)
+Operating System :: OS Independent
+Topic :: Software Development :: Libraries :: Python Modules
+Topic :: Communications :: Chat
+Topic :: Internet :: WWW/HTTP :: Indexing/Search
+Topic :: Software Development :: Quality Assurance
+Topic :: System :: Monitorings
+"""
+
 setup(name='gitorious-mrq-monitor',
     version='0.0.1',
     description='Monitor Gitorious merge requests over IRC',
@@ -10,4 +24,5 @@ setup(name='gitorious-mrq-monitor',
     url='https://github.com/jonnor/gitorious-mrq-monitor',
     packages=['gitorious_mrq'],
     scripts=['bin/gitorious-mrq-monitor'],
+    classifiers=classifiers,
 )
